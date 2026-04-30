5a2cd6b1fdb1664c17b3229ff8f907e137fc3b74
Alana Pooler <apooler@ncsu.edu>
2026-04-30 11:46:01 -0400

    Updating narrative and comments

--- data_production.py
+++ data_production.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import time
 
+# read in power streaming data
 df = pd.read_csv("data/power_streaming_data.csv")
 
 for i in range(20):
 
+    # take random sample of 5 values
     sample_df = df.sample(5)
 
+    # output sample to folder
     sample_df.to_csv(
         f"stream_output/batch_{i}.csv",
         index=False
@@ -14,4 +17,5 @@ for i in range(20):
 
     print(f"Wrote batch {i}")
 
+    # wait for 10 seconds before next iteration
     time.sleep(10)
